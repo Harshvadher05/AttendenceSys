a3009ce6d6da2756baacc8f3d91d63dcb1e326c9
Harsh Vadher <153319594+Harshvadher05@users.noreply.github.com>
2025-04-04 11:54:28 +0530

    n2

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,9 +2,6 @@ import React from "react";
 
 export default function Home() {
   return (
-    <iframe
-      src="AttendenceSys\src\components\Home\homepage.html"
-      frameborder="0"
-    ></iframe>
+   <div></div>
   );
 }
